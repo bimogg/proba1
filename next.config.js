06006ccd31a9199b68e03184f4735c41f518cd62
Alina <alina@MacBook-Pro.local>
2025-12-02 18:04:40 +0500

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/proba1',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
